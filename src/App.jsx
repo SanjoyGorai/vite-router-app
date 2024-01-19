1f167decd0cd6778418390afb32e8baa211fdfaf
Mr. Sanjoy <--add>
2024-01-19 16:09:16 +0530

    lazy added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './Componants/Navbar/Navbar'
 import Home from './Componants/Home/Home'
 import { Outlet } from "react-router-dom";
 import Footer from './Componants/Footer/Footer';
+import { lazy } from 'react';
+
+// const  = lazy(()=> import(''))
 
 function App() {
 
